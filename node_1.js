7900f810713856ac55b6c9a208cc3ebb3b6b2cb9
Benjamin Patrick <61899689+bhenjameen@users.noreply.github.com>
2020-03-30 23:21:13 +0100

    Node JS 1

---
+++ node_1.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+    fs.readFile('test.html', function(err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+
+    let data = req.data;
+    fs.appendFileSync('./message.txt', data);
+
+}).listen(8080);
